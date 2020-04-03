361309d3f95358210bd5d6224ce20a6674da9a20
Ylihärsilä <Mikko.Yliharsila@seamk.fi>
2020-04-03 20:33:58 +0300

    commit

--- material_controller.js
+++ material_controller.js
@@ -1,10 +1,29 @@
+const material_model = require('./material_model');
+
 // CREATE
 
 const api_post_material = (req, res, next) => {
     console.log('api_post_material');
-    let data = req.body;
-    console.log(data);
-    res.send(JSON.stringify(data));
+    let data = {
+        name: req.body.name,
+        min_density: req.body.min_density,
+        max_density: req.body.max_density,
+        min_strength: req.body.min_strength,
+        max_strength: req.body.max_strength,
+        min_strength_density: req.body.min_strength / req.body.max_density,
+        max_strength_density: req.body.max_strength / req.body.min_density
+    };
+
+    let new_material = material_model(data);
+
+    new_material.save().then(() => {
+        console.log(new_material);
+        res.send(JSON.stringify(new_material));
+    }).catch(err => {
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
 
 };
 
@@ -12,8 +31,16 @@ const api_post_material = (req, res, next) => {
 
 const api_get_materials = (req, res, next) => {
     console.log('api_get_materials');
-    res.send(JSON.stringify([]));
 
+    material_model.find({})
+        .lean()
+        .then(materials => {
+            res.send(JSON.stringify(materials));
+        }).catch(err => {
+            res.status(500);
+            res.send(err.errmsg);
+            console.log(err);
+        });
 };
 
 // UPDATE
