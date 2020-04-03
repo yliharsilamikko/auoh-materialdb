cc04fc382c6a680405c09e2609febd85cf17e1c0
Ylihärsilä <Mikko.Yliharsila@seamk.fi>
2020-04-03 19:03:04 +0300

    commit

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 
 const body_parser = require('body-parser');
 
+const material_controller = require('./material_controller');
+
+// npm init
+// npm install express
+// npm install nodemon --save-dev
+// npm run start-dev
+
 app.use(body_parser.json()); //req.body.name
 app.use(body_parser.urlencoded({
     extended: true
@@ -11,7 +18,21 @@ app.use(body_parser.urlencoded({
 
 app.use((req, res, next) => {
     console.log(req.method, ' ', req.path);
+    next();
 }); // GET /api/materials
 
+
+// RESTful API
+// CRUD OPERATIONS
+
+//CREATE
+app.post("/api/material", material_controller.api_post_material);
+
+//api.domain.com/materials
+// READ
+app.get("/api/materials", material_controller.api_get_materials);
+// UPDATE
+
+// DELETE
 
 app.listen(port);
--- material_controller.js
+++ material_controller.js
@@ -1,8 +1,17 @@
 // CREATE
 
+const api_post_material = (req, res, next) => {
+    console.log('api_post_material');
+    let data = req.body;
+    console.log(data);
+    res.send(JSON.stringify(data));
+
+};
+
 // READ
 
 const api_get_materials = (req, res, next) => {
+    console.log('api_get_materials');
     res.send(JSON.stringify([]));
 
 };
@@ -13,4 +22,5 @@ const api_get_materials = (req, res, next) => {
 
 
 // EXPORTS
+module.exports.api_post_material = api_post_material;
 module.exports.api_get_materials = api_get_materials;
